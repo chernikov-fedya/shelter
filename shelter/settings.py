fc700c8f6cd877ddf12b1bd137aa9c9fcc947e15
Polina_1441 <14042003dp@mail.ru>
2020-10-12 18:12:22 +0700

    static

--- shelter/settings.py
+++ shelter/settings.py
@@ -54,12 +54,13 @@ ROOT_URLCONF = 'shelter.urls'
 
 STATIC_URL = '/static/'
 
-TEMPLATE_DIRS = os.path.join(BASE_DIR, "static")
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATE_DIRS]
+        'DIRS': [STATICFILES_DIRS]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
@@ -123,8 +124,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
