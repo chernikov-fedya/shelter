cadc04b5d1585ba0fcf1481b5e0e0d6ce4397822
Daniil325 <73283825+Daniil325@users.noreply.github.com>
2020-10-27 18:15:57 +0700

    Update settings.py

--- shelter/settings.py
+++ shelter/settings.py
@@ -120,3 +120,5 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
