611c7cd5dd376b0175865bbfa43a1efa849cf686
Daniil325 <73283825+Daniil325@users.noreply.github.com>
2020-11-03 15:30:25 +0700

    поиск животных

--- appShelter/views.py
+++ appShelter/views.py
@@ -1,10 +1,14 @@
-from random import Random
+from appShelter.models import *
+from django.db.models import Q
 from django.shortcuts import render, get_object_or_404
-from appShelter.models import Animal
-
 
 def index(request):
-    all_animal = Animal.objects.all()
+    search_query = request.GET.get('search', '')#поиск_животных
+    if search_query:
+        all_animal = Animal.objects.filter(Q(name__icontains=search_query) | Q(description__icontains=search_query)|
+                                           Q(age__name__icontains=search_query))
+    else:
+        all_animal = Animal.objects.all()
     context = {'all_animal': all_animal}
     return render(request, "index.html", context)
 
